0c6092aa5adfe29bc76df9c777b78020c14d6c3f
Hurt Luis <Administrator@lettery.onmicrosoft.com>
2026-09-13 20:02:55 +0800

    更新样式和音乐

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -48,10 +48,10 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				name: "皎洁的笑颜 Moonlike Smile",
+				artist: "陈致逸,HOYO-MiX",
+				url: "/assets/music/陈致逸,HOYO-MiX - Moonlike Smile 皎洁的笑颜.flac",
+				cover: "https://p1.music.126.net/_U__1_DT6eLbvObNtK3apg==/109951165849462124.jpg",
 				lrc: "",
 			},
 		],
